d4b2f656d72dea1d3539372f7d32132f371d1ce1
Ваше Имя <you@example.com>
2019-11-07 13:04:01 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <Menu/>
+  </div>
+</template>
+
+<script>
+import Menu from './components/Menu.vue'
+
+export default {
+  name: 'App',
+  components: {
+     Menu
+  }
+}
+</script>
+
+<style>
+  body{
+      padding: 0;
+      margin: 0;
+      font-family: Helvetica, sans-serif;
+      background-color: #f5f5f5;
+  }
+</style>
